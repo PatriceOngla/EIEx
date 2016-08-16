193d18da4534244f0fcad5d19ceef1b6cfa7dc6e
Patrice Ongla <contact-ongla@leg-si.com>
2016-08-16 12:10:22 +0200

    Complément du commit précédent (ajout de commentaires dans le code).

--- EIEx/EIEx/Code/Vue/Référentiel/UC_SélecteurDeProduit.xaml.vb
+++ EIEx/EIEx/Code/Vue/Référentiel/UC_SélecteurDeProduit.xaml.vb
@@ -581,17 +581,13 @@ Public Class UC_SélecteurDeProduit
 
 #End Region
 
-    'TODO: J'ai tenté d'appliquer http://stackoverflow.com/questions/5869359/wpf-modeless-dialog-from-ms-excel-add-in mais sans succès car on se retrouve dans une situation bancale où la fenêtre doit être impérativement créée dans l'autre thread (pour le pb de keyboard focus) mais ou son content (le UserControl) est créé dans le thread appelant.
+    'TODO: J'ai tenté d'appliquer http://stackoverflow.com/questions/5869359/wpf-modeless-dialog-from-ms-excel-add-in mais sans succès car on se retrouve dans une situation bancale où la fenêtre doit être impérativement créée dans l'autre thread (pour le pb de keyboard focus) mais ou son content (le UserControl) est créé dans le thread appelant. Y'a aussi http://stackoverflow.com/questions/19458721/cant-type-on-a-wpf-window-in-a-vsto-addin mais je le sens pas. On abandonne plutôt le principe de l'addin qui n'est finalement pas pertinent. 
+
 
 #Region "Show"
 
     Private Shared Sub Show_Core()
-        Dim T As New Thread(Sub()
-                                System.Diagnostics.Debug.Print("Toto")
-                            End Sub)
-
-        T.Start()
-
+        'Pas d'erreur d'exécution mais ne montre rien ! Je pense que c'est du au STA qui isole complétement le thread. Après exécution, l'objet reste de toute façon innaccessible directement depuis le reste du code qui tourne dans le thread principal. Donc galère. 
         Dim ShowTask As New Thread(Sub()
                                        _Instance = New UC_SélecteurDeProduit()
                                        _Instance.Margin = New Thickness(0)
